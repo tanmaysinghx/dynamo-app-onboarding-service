8a648d14bad2b25d19d61c69297801446a9492f5
Tanmay Singh <tanmaysinghx@gmail.com>
2024-02-07 19:24:24 +0530

    Added swagger

--- src/main/java/com/dynamoapp/controller/CompanyDetailsController.java
+++ src/main/java/com/dynamoapp/controller/CompanyDetailsController.java
@@ -10,14 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dynamoapp.model.CompanyDetails;
 import com.dynamoapp.service.CompanyDetailsService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/company-details")
 @CrossOrigin("*")
+@Tag(description = "Your API Description", name = "Company Details")
 public class CompanyDetailsController {
 	
 	@Autowired 
 	private CompanyDetailsService companyDetailsService;
 	
+	@Operation(summary = "Api to register company", description = "")
 	@PostMapping("/register-company")
 	public CompanyDetails registerCompanyDetails(@RequestBody CompanyDetails companyDetails) throws Exception {
 		return this.companyDetailsService.registerCompany(companyDetails);
